75a3a12fe67b250e1995633467a308489af74438
Samer <sameralamayreh01@gmail.com>
2020-02-28 10:20:52 +0200

    init commit

--- src/main/java/Task1/Test.java
+++ src/main/java/Task1/Test.java
@@ -4,10 +4,11 @@ public class Test {
     public static void main(String[] args) {
         String A = "cata";
         String B = "taca";
+        String C = "aact";
 
         Anagram anagram = new Anagram();
 
-        System.out.println(anagram.anagram(A, B));
+        System.out.println(anagram.anagram(A, B, C));
 
 
 //        Anagram2 anagram2=new Anagram2();
